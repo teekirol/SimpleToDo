c206b6032f4f49f463920d5722fd8ae40ff1b802
Lori Lee <leelorik@gmail.com>
2014-12-27 12:16:17 -0500

    SQLite db schema

---
+++ app/src/main/java/com/codepath/simpletodo/ToDoContract.java
@@ -0,0 +1,17 @@
+package com.codepath.simpletodo;
+
+import android.provider.BaseColumns;
+
+public final class ToDoContract {
+
+    public ToDoContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class ToDoEntry implements BaseColumns {
+        public static final String TABLE_NAME = "todos";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_POSITION = "position";
+        public static final String COLUMN_NAME_TEXT = "text";
+        public static final String COLUMN_NAME_COMPLETED = "completed";
+    }
+}
---
+++ app/src/main/java/com/codepath/simpletodo/ToDoDbHelper.java
@@ -0,0 +1,36 @@
+package com.codepath.simpletodo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.codepath.simpletodo.ToDoContract.ToDoEntry;
+
+public class ToDoDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "ToDo.db";
+
+    public ToDoDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+            "CREATE TABLE " + ToDoEntry.TABLE_NAME + "(" +
+            ToDoEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
+            ToDoEntry.COLUMN_NAME_TEXT + " TEXT," +
+            ToDoEntry.COLUMN_NAME_POSITION + " INTEGER," +
+            ToDoEntry.COLUMN_NAME_COMPLETED + " BOOLEAN)"
+        );
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Copy table content into new db
+        db.execSQL("DROP TABLE IF EXISTS " + ToDoEntry.TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
